92424828c5a24f525a9fe0e0a261681768b9601d
Juliovsk1 <jcezarmaricaua@gmail.com>
2024-11-30 21:01:57 -0300

    nova versao

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
+import Link from 'next/link'
+
 export default function Homepage(){
     return(
         <div>
              <h1>Alura - Home</h1>
-             <a href="/faq">Ir para o FAQ</a>
+             <Link href="/faq">
+                <a>Ir para o FAQ</a>
+             </Link>
         </div>
     )
 }
