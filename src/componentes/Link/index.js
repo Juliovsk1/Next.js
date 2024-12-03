df87f2333a80466c86a3b69a606df63c388e03a6
Juliovsk1 <jcezarmaricaua@gmail.com>
2024-12-02 21:50:25 -0300

    funcionando

--- src/componentes/Link/index.js
+++ src/componentes/Link/index.js
@@ -2,7 +2,7 @@ import NextLink from "next/link";
 
 export default function Link({ children, href, ...props}) {
     return (
-        (<NextLink href={href} passhref {...props}>
+        (<NextLink href={href} passhref {...props} legacyBehavior>
             {children}
         </NextLink>)
     );
